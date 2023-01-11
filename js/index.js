616628fc421408fb94496a7a38c749718761500d
aklekotka2 <aklekotka@interia.pl>
2023-01-11 21:39:15 +0100

    Projekt JS nr 2, wersja 3 - dodano obsuge bledow ( UserExpirence, catch, finally)

--- js/index.js
+++ js/index.js
@@ -1,24 +1,41 @@
 const button = document.querySelector("#getCurrencies");
 const input = document.querySelector("#amount");
 const select = document.querySelector('#currency');
-let selectedCurrency = select.options[select.selectedIndex].value;
+let selectedCurrency;
 let exchangeValue;
 const resultArea = document.querySelector('#result span');
+const errArea = document.querySelector('.err');
+const apiErrArea = document.querySelector('.api-err');
 
+function validate(val){
+    if(val < 0){
+        errArea.innerText = 'Wartość musi być dodatnia.';
+        return false;
+    }
+    errArea.innerText = '';
+    return true;
+}
 
 function exchange(currencies){
-    exchangeValue = currencies * input.value;
-    resultArea.innerText = Number(Math.round(exchangeValue + 'e+2') + 'e-2');;
+    if( validate(input.value)){
+        exchangeValue = currencies * input.value;
+        resultArea.innerText = exchangeValue.toFixed(2);
+    }
+    else{
+        resultArea.innerText = 0;
+    }  
 }
 function getCurrencyList(e){
     e.preventDefault();
+    selectedCurrency = select.options[select.selectedIndex].value;
     const urlAddress = `https://api.nbp.pl/api/exchangerates/rates/a/${selectedCurrency}/`;
 
     document.querySelector('.loader').classList.remove('invisible');
     fetch(urlAddress)
     .then((response) => response.json())
-    .then((data) => exchange(data.rates[0].mid));
-    setTimeout(()=>{ document.querySelector('.loader').classList.add('invisible'); }, 3000 );
+    .then((data) => exchange(data.rates[0].mid))
+    .catch(error => apiErrArea.innerText = `Nastąpił błąd: ${error}`)
+    .finally(document.querySelector('.loader').classList.add('invisible')); 
 }
 
 button.addEventListener('click', (e) => getCurrencyList(e));
