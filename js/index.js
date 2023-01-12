e3a67b939e5ecd007cc6a319dbd88944ebe2967f
aklekotka2 <aklekotka@interia.pl>
2023-01-12 21:15:31 +0100

    Projekt JS nr 2, werja 3 poprawiony

--- js/index.js
+++ js/index.js
@@ -1,41 +1,40 @@
 const button = document.querySelector("#getCurrencies");
 const input = document.querySelector("#amount");
-const select = document.querySelector('#currency');
+const select = document.querySelector("#currency");
 let selectedCurrency;
 let exchangeValue;
-const resultArea = document.querySelector('#result span');
-const errArea = document.querySelector('.err');
-const apiErrArea = document.querySelector('.api-err');
+const resultArea = document.querySelector("#result span");
+const errArea = document.querySelector(".err");
+const apiErrArea = document.querySelector(".api-err");
 
-function validate(val){
-    if(val < 0){
-        errArea.innerText = 'Wartość musi być dodatnia.';
-        return false;
-    }
-    errArea.innerText = '';
-    return true;
+function validate(val) {
+	if (val < 0) {
+		errArea.innerText = "Wartość musi być dodatnia.";
+		return false;
+	}
+	errArea.innerText = "";
+	return true;
 }
 
-function exchange(currencies){
-    if( validate(input.value)){
-        exchangeValue = currencies * input.value;
-        resultArea.innerText = exchangeValue.toFixed(2);
-    }
-    else{
-        resultArea.innerText = 0;
-    }  
+function exchange(currencies) {
+	if (validate(input.value)) {
+		exchangeValue = currencies * input.value;
+		resultArea.innerText = exchangeValue.toFixed(2);
+	} else {
+		resultArea.innerText = 0;
+	}
 }
-function getCurrencyList(e){
-    e.preventDefault();
-    selectedCurrency = select.options[select.selectedIndex].value;
-    const urlAddress = `https://api.nbp.pl/api/exchangerates/rates/a/${selectedCurrency}/`;
+function getCurrencyList(e) {
+	e.preventDefault();
+	selectedCurrency = select.options[select.selectedIndex].value;
+	const urlAddress = `https://api.nbp.pl/api/exchangerates/rates/a/${selectedCurrency}/`;
 
-    document.querySelector('.loader').classList.remove('invisible');
-    fetch(urlAddress)
-    .then((response) => response.json())
-    .then((data) => exchange(data.rates[0].mid))
-    .catch(error => apiErrArea.innerText = `Nastąpił błąd: ${error}`)
-    .finally(document.querySelector('.loader').classList.add('invisible')); 
+	document.querySelector(".loader").classList.remove("invisible");
+	fetch(urlAddress)
+		.then((response) => response.json())
+		.then((data) => exchange(data.rates[0].mid))
+		.catch((error) => (apiErrArea.innerText = `Nastąpił błąd: ${error}`))
+		.finally(document.querySelector(".loader").classList.add("invisible"));
 }
 
-button.addEventListener('click', (e) => getCurrencyList(e));
+button.addEventListener("click", (e) => getCurrencyList(e));
